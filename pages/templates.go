50f5c3faa657f00102ae7496454f395ff0d13467
D <code@dector.space>
2025-07-11 23:58:28 +0200

    wip: add `../` to navigation
    Co-authored-by: DBot <codebot@dector.space>

--- pages/templates.go
+++ pages/templates.go
@@ -86,6 +86,18 @@ func GenerateFolderPage(node *fs.FsNode, relativePath string) []byte {
 		return dirEntries[i].Name < dirEntries[j].Name
 	})
 
+	// Add parent directory entry if not in root
+	if relativePath != "" && relativePath != "." {
+		parentEntry := DirectoryEntry{
+			Name:     "..",
+			IsDir:    true,
+			Icon:     template.HTML(`<svg class="icon" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path></svg>`),
+			Suffix:   "/",
+			CSSClass: "dir",
+		}
+		dirEntries = append([]DirectoryEntry{parentEntry}, dirEntries...)
+	}
+
 	displayPath := relativePath
 	if displayPath == "" || displayPath == "." {
 		displayPath = "./"
